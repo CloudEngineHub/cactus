0ff58d55d3d645f45bc995ecfa1141a3708c5830
HMUNACHI <ndubuakuhenry@gmail.com>
2025-08-16 17:18:49 -0700

    publish latest xcframework and jnilibs
    Former-commit-id: 45781453590c9edaf7b91de1518843f2d0b35c82 [formerly 526e9cbadec2298d325d605ca482784cdcb3a30a]
    Former-commit-id: 6d215e91fbc9eb9ffcef5597e158752b250222f6

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,16 +5,10 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ROOT_DIR="$(cd "$SCRIPT_DIR/.." && pwd)"
 
 "$SCRIPT_DIR/build-ios.sh"
-# "$SCRIPT_DIR/build-react-android.sh"
-"$SCRIPT_DIR/build-flutter-android.sh"
-
-rm -rf "$ROOT_DIR/flutter/android/jniLibs.zip"
-rm -rf "$ROOT_DIR/flutter/android/src/main/jniLibs/x86_64" 
+"$SCRIPT_DIR/build-react-android.sh"
 rm -rf "$ROOT_DIR/react/android/src/main/jniLibs/x86_64"
 
-# "$SCRIPT_DIR/build-react.sh"
-"$SCRIPT_DIR/build-flutter.sh"
-
+"$SCRIPT_DIR/build-react.sh"
 cd "$ROOT_DIR/react"
 npm version patch
 npm publish
@@ -24,5 +18,10 @@ git add .
 git commit -m "publish latest xcframework and jnilibs"
 git push origin main
 
-cd "$ROOT_DIR/flutter"
-flutter pub publish 
+# "$SCRIPT_DIR/build-flutter-android.sh"
+# rm -rf "$ROOT_DIR/flutter/android/jniLibs.zip"
+# rm -rf "$ROOT_DIR/flutter/android/src/main/jniLibs/x86_64" 
+
+# "$SCRIPT_DIR/build-flutter.sh"
+# cd "$ROOT_DIR/flutter"
+# flutter pub publish 
